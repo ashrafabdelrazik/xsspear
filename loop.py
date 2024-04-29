985f5ad449e926ba3248833dd4a68f25d81851cf
Ashraf Abdelrazik <49130788+ashrafabdelrazik@users.noreply.github.com>
2024-04-29 15:37:36 -0500

    Update loop.py

--- loop.py
+++ loop.py
@@ -11,15 +11,13 @@ import re
 
 def loop(parameters, headers, url):
 
-  q = "=xsspear<>"
-  out = open('spear.out', 'a+')
+  q = "=xsspear<>&"
+  out = open('xsspear.out', 'a+')
 
-  print("\n\033[96m[!] Requests: \033[33m%s" % len(p))
+  print("\n\033[96m[!] Requests: \033[33m%s" % math.trunc(len(p)/15))
   print("\033[96m[!] Scanning: \033[33m%s" % url)
 
-  with tqdm(total=len(p),
-            desc="\033[96m[!] RXSS Scan",
-            bar_format="\033[33m{l_bar}{bar}") as pbar:
+  with tqdm(total=math.trunc(len(p)/15), desc="\033[96m[!] RXSS Scan", bar_format="\033[33m{l_bar}{bar}") as pbar:
     o = set()
 
     async def main(p):
@@ -38,7 +36,7 @@ def loop(parameters, headers, url):
 
       async with aiohttp.ClientSession() as session:
         ret = await asyncio.gather(
-            *[get(url + "?&" + p + q, session) for p in parameters])
+            *[get(url + "?&" + p1 + q + p2 + q + p3 + q + p4 + q + p5 + q + p6 + q + p7 + q + p8 + q + p9 + q + p10 + q + p11 + q + p12 + q + p13 + q + p14 + q + p15 + q, session) for p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15 in zip(parameters,parameters,parameters,parameters,parameters,parameters,parameters,parameters,parameters,parameters,parameters,parameters,parameters,parameters,parameters)])
 
     loop = asyncio.get_event_loop()
     start = time.time()
@@ -48,26 +46,25 @@ def loop(parameters, headers, url):
   out.close()
 
   print("\033[96m[!] Duration: \033[33m%s\033[96ms" % math.trunc(end - start))
-  print("\033[96m[!] Reflections: \033[31m%s" %
-        str(len(open('spear.out', 'r').readlines())))
+#  print("\033[96m[!] Reflections: \033[31m%s" %str(len(open('xsspear.out', 'r').readlines())))
 
   if len(o) == 0:
     print("\033[96m[!] \033[33mSecure")
   else:
     print("\033[96m[+] \033[31mInsecure")
-    message = EmailMessage()
-    message.set_content(
-        '<h3>URLS:</h3><br><code> ' + str(list(o)),
-        subtype='html')
+#    message = EmailMessage()
+#    message.set_content(
+#        '<h3>URLS:</h3><br><code> ' + str(list(o)),
+#        subtype='html')
 
-    message['Subject'] = '[XSSpear] Possible rXSS on ' + url
-    message['From'] = 'null'
-    message['To'] = 'null'
-    try:
-      server = smtplib.SMTP('smtp.office365.com', 587)
-      server.starttls()
-      server.login('null', 'null')
-      server.send_message(message)
-      server.quit()
-    except Exception as e:
-      print('Error sending email:', e)
+#    message['Subject'] = '[XSSpear] Possible rXSS on ' + url
+#    message['From'] = 'ashrafabdelrazikato@hotmail.com'
+#    message['To'] = 'ashrafabdelrazik1996@gmail.com'
+#    try:
+#      server = smtplib.SMTP('smtp.office365.com', 587)
+#      server.starttls()
+#      server.login('ashrafabdelrazikato@hotmail.com', 'Aliaa2309!x')
+#      server.send_message(message)
+#      server.quit()
+#    except Exception as e:
+#      print('Error sending email:', e)
